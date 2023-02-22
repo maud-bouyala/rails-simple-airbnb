0111bc52538c61a616e1ba401ee05ae8d48308dd
maud bouyala <maud.bouyala@yahoo.fr>
2023-02-22 17:05:03 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Super cool beach flat',
+  address: '22 rue de la Paix Port Camargues',
+  description: 'A nice 2bedrooms flat with a nice terrasse above the port',
+  price_per_night: 80,
+  number_of_guests: 6
+)
+
+
